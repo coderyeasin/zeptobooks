029229bde60cecc01df80ba831af4f6428877d1e
coderyeasin <yeasin3233@gmail.com>
2024-10-18 15:54:02 +0600

    data-loading-by-card

---
+++ js/main.js
@@ -0,0 +1,48 @@
+const loadingSpinner = document.getElementById('loading');
+const displayData = document.getElementById('displayData');
+const searchBox = document.getElementById('searchId');
+const filterByGenre = document.getElementById('genreFilter');
+
+// Fetch data and display loading
+const displayDataOnWeb = async () => {
+    try {
+        loadingSpinner.style.display = 'block';
+        const res = await fetch('https://gutendex.com/books');
+        const data = await res.json();
+        createBookCard(data.results);
+        console.log('data', data.results);
+        loadingSpinner.style.display = 'none';
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        loadingSpinner.innerHTML = 'Error loading data.';
+    }
+};
+
+// Create book card
+function createBookCard(books) { 
+    displayData.innerHTML = '';
+
+    for (const book of books) {
+        const bookDiv = document.createElement('div');
+        bookDiv.className = 'book-card';
+        bookDiv.innerHTML = `
+            <div class="bookCover" onclick="goToBookPage(${book.id})"> 
+                <img class="" src="${book.formats['image/jpeg'] || ''}" alt="${book.title || 'Book Cover'}">
+                <div class="book-content">
+                    <h3>${book.title.slice(0, 70) || 'Untitled'}</h3>
+                    <p><strong>Book ID: </strong> ${book.id}</p>
+                    <p><strong>Author:</strong> ${book.authors.length > 0 ? book.authors[0].name : 'Unknown Author'}</p>
+                    <p><strong>Genre:</strong> ${book.subjects ? book.subjects.join(', ').slice(0, 40) : 'No Genres Available'}</p>
+                </div>
+                <div class="book-btn">
+                    <button class="details" onclick="goToBookPage(${book.id})"> View Details</button>
+                    <button class="wishlist" onclick="toggleWishlist(${book.id})"> Add to Wishlist </button>
+                </div>
+            </div>
+        `;
+        displayData.appendChild(bookDiv);  
+    }
+}
+
+// Call the function to display data on web
+displayDataOnWeb();
