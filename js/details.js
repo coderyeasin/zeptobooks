3c994d1fc2ec78963894fa3cdabd7ba38f9bd371
coderyeasin <yeasin3233@gmail.com>
2024-10-24 21:40:37 +0600

    fix-details

--- js/details.js
+++ js/details.js
@@ -19,3 +19,9 @@ if (currentBook) {
 } else {
     bookDetail.innerHTML = '<p>No book selected.</p>';
 }
+
+// For hamburger
+function toggleMenu() {
+    const menu = document.querySelector('.nav-menu');
+    menu.classList.toggle('active');
+}
